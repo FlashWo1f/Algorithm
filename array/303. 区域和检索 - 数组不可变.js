517188798372386260e908dc80106cd71139fce4
Libin <1064048947@qq.com>
2022-06-23 17:18:15 +0800

    readme && 303区域和检索

---
+++ array/303. 区域和检索 - 数组不可变.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function (nums) {
+  this.nums = nums
+  let pre = nums[0]
+  this.preSums = nums.map((i, idx) => {
+    const res = idx ? i + pre : i
+    return pre = res
+  })
+};
+
+/** 
+ * @param {number} left 
+ * @param {number} right
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function (left, right) {
+  return this.preSums[right] - (left ? this.preSums[left - 1] : 0)
+};
+
+const nums = [-2, 0, 3, -5, 2, -1]
+var obj = new NumArray(nums)
+console.log(obj.preSums)
+console.log(obj.sumRange(0, 2))
+console.log(obj.sumRange(2, 5))
+console.log(obj.sumRange(0, 5))
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
