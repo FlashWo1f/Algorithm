84e328ff0f9f5a9ed773361cf08ea07543065b23
Libin <1064048947@qq.com>
2022-09-01 15:54:46 +0800

    无重复子串：for版本

--- 滑动窗口/3.无重复字符的最长子串.js
+++ 滑动窗口/3.无重复字符的最长子串.js
@@ -6,17 +6,17 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
+var lengthOfLongestSubstring = function (s) {
   let res = 0
   let left = 0
   let right = 0
   let window = {}
-  while(right < s.length) {
+  while (right < s.length) {
     const rightChar = s[right]
     right++
     window[rightChar] = window[rightChar] ? window[rightChar] + 1 : 1
     // 找到解后，尝试优化解
-    while(window[rightChar] > 1) {
+    while (window[rightChar] > 1) {
       const leftChar = s[left]
       left++
       window[leftChar]--
@@ -26,4 +26,27 @@ var lengthOfLongestSubstring = function(s) {
   return res
 }
 
-console.log(lengthOfLongestSubstring("abcdaczzzsvcabcbb"))
+var lengthOfLongestSubstring1 = function (s) {
+  if (s === '') return 0
+  let ans = 1
+  for (let i = 0; i < s.length; i++) {
+    let res = 1
+    let l = i, r = i + 1
+    for (; r < s.length; r++) {
+      if (s.slice(l, l + res).includes(s[r])) {
+        break
+      }
+      res += 1
+    }
+    ans = Math.max(ans, res)
+  }
+  return ans
+};
+const str = 'abcdaczzzsvcabcbbsdfzxcvdasdsdfewrsadadvfxzccxvasdfdczcxvsdafsadeddasfasfawfasdfageadsvdflikhasdlkfjvabsfbasjiklvbhaisbvilqebfvasuivbqeirhfiqwbdvbasdjvbsavnbkjasbvhisqnvjerhoeuwhviuhfdlkvahsflvhasviboyeqhvolanxckvnbqeoviqnroghietuygtgynasifnasughqiornvoqsvnojqweboprqvnoqbvaashfdbaOIUGBOEJABGBasbasadifzkjdfbaifpewbfZkjvbcsvbausdhrfeuwlksngoagnbprbgoqgouasbdgpiabgb'
+console.time('while')
+console.log(lengthOfLongestSubstring(str))
+console.timeEnd('while')
+
+console.time('for')
+console.log(lengthOfLongestSubstring1(str))
+console.timeEnd('for')
