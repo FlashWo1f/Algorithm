6867daddebb764be4f7e1ebf50371201e213948b
Libin <1064048947@qq.com>
2022-09-04 16:23:21 +0800

    整理文件

--- linkedList/142.环形链表2.js
+++ linkedList/142.环形链表2.js
@@ -1,18 +1,37 @@
-var detectCycle = function(head) {
+/*
+ * @lc app=leetcode.cn id=142 lang=javascript
+ *
+ * [142] 环形链表 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function (head) {
   if (!head || !head.next) return null
   let slow = head, fast = head
-  while(fast && fast.next) {
-      fast = fast.next.next
-      slow = slow.next
-      if (fast === slow) break
+  while (fast && fast.next) {
+    fast = fast.next.next
+    slow = slow.next
+    if (fast === slow) break
   }
   if (!fast || !fast.next) return null
   slow = head
   console.log(slow, fast, slow !== fast)
-  while(slow !== fast) {
+  while (slow !== fast) {
     console.log('do')
-      slow = slow.next
-      fast = fast.next
+    slow = slow.next
+    fast = fast.next
   }
   console.log(slow, fast)
   return slow
--- linkedList/206.反转链表.js
+++ linkedList/206.反转链表.js
@@ -50,3 +50,14 @@ while (head) {
   console.log(head.val)
   head = head.next
 }
+
+var reverseList1 = function(head) {
+  if (!head || !head.next) return head 
+  let tail = head.next
+  const p = reverseList1(head.next)
+  // p 始终为 这条原始链表的最后一个节点(反转后的head节点)
+  // tail.next 也始终为 null  所以这道题 tail.next 可以用 null 代替
+  head.next = tail.next
+  tail.next = head
+  return p
+};
